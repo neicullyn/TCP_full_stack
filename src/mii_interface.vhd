429575237bfa19eb3d602c7ab8f5d2ae0f18bb00
christineLydia <liluxiaolizi@gmail.com>
2015-06-04 21:38:22 -0700

    mii_interface

--- src/mii_interface.vhd
+++ src/mii_interface.vhd
@@ -29,7 +29,7 @@ port(
 			RXCLK : in std_logic;
 			nRST: in std_logic;			
 
-			TX_DV: in std_logic; -- from MAC, 
+			TXDV: in std_logic; -- from MAC, 
 			-- TX_DV is given by MAC to decide whether we need to transmit(read out) data from MAC to PHY
 			-- when all the data is read out from MAC, it will become invalid which is set by MAC
 			TXEN: out std_logic;--transmit enable
@@ -37,127 +37,147 @@ port(
 		
 			RXDV: in std_logic;--receive valid(enable)
 			
-			TX_in: in std_logic_vector(3 downto 0);-- transmit mode: data input from MAC
+			TX_in: in std_logic_vector(7 downto 0);-- transmit mode: data input from MAC
 			TXD: out std_logic_vector(3 downto 0); -- transmit mode: data output to PHY
 			
 			RXD: in std_logic_vector(3 downto 0);-- receive mode: data input from PHY			
-			RX_out: out std_logic_vector(3 downto 0);-- receve mode: data output to MAC
+			RX_out: out std_logic_vector(7 downto 0);-- receve mode: data output to MAC
 			
 			WR: out std_logic;-- receive mode: to push a 4-bit data into FIFO, active high
 			RD: out std_logic-- transmit mode: to pop a 4-bit data out of FIFO, active high
-			-- WR and RD is the control signal inside MAC specifically to update the FIFO
-			-- WR: in receive mode to push data into FIFO, when writing and updating in the same state
-			-- RD: in transmit mode to pop data out of FIFO, reading out first and in the next state, we update the FIFO
 	 );
 end mii_interface;
 
 architecture Behavioral of mii_interface is
---FSM
-	type states is (
-		IDLE,
-		State1,
-		State2);
--- the FSM is only used in transmit mode
+	
+	-- counters for both directions
+	type states is (Idle, First, Second);
+	signal TX_state : states := Idle;
+	signal RX_state : states := Idle;
 
-	signal state_current: states:=IDLE;
+	-- data registers
+	signal TX_register : STD_LOGIC_VECTOR(7 downto 0);
+	signal RX_register : STD_LOGIC_VECTOR(7 downto 0);
+	
+	-- to generate local clocks
+	signal RXCLK_prev :  std_logic; 
+	signal TXCLK_prev: std_logic; 
+	signal fall_RXCLK:  std_logic;  
+	signal fall_TXCLK: std_logic;  
 
 	
-	signal RXCLK_pre :  std_logic; -- used as output of DFF to generate a local RX clock for MII
-	signal TXCLK_pre: std_logic; -- used as output of DFF to generate a local TX clock for MII
-	signal fall_RCLK:  std_logic; -- 
-	signal fall_TCLK: std_logic; -- 
-	
-
-	
-
 begin
-		
-		
-		TXD<=TX_in; -- act as the data bus, they are always connected
-		RX_out<=RXD;
-	
-	-- 	
-	p1: process(CLK)
-	begin
-		if rising_edge(CLK) then
-			RXCLK_pre<=RXCLK;
-		end if;
-   end process p1;
-		
-	p2: process(CLK)
-	begin
-		if rising_edge(CLK) then
-		TXCLK_pre<=TXCLK;
-		end if;
-	end process p2;
-		
-	
-	-- to generate a fall_CLK at the falling edge of RXCLK
-	-- This is to drive WR signal
-	-- receive mode: PHY to MAC
-		process(RXCLK,RXCLK_pre)
-		begin
-		if RXCLK='0' and RXCLK_pre='1' then
-			fall_RCLK<='1';
-		else 
-			fall_RCLK<='0';
-		end if ;
-		end process;
-		
-		-- main process for receive mode
-		process(fall_RCLK,RXDV)
-		begin
-		if (fall_RCLK='1') and (RXDV='1') then
-			WR<='1'; -- WR valid
-		else 
-			WR<='0';
-		end if;
-		end process;
 
-	-- to generate a fall_TCLK at the falling edge of TXCLK
-	-- This is to drive RD signal
-		process(TXCLK,TXCLK_pre)
-		begin
-		if TXCLK='0' and TXCLK_pre='1' then
-		fall_TCLK<='1';
+	TX_register <= TX_in;
+	RX_out <= RX_register;
+	
+	
+	process(CLK)
+	begin
+		if (rising_edge(CLK)) then
+			RXCLK_prev <= RXCLK;
+			TXCLK_prev <= TXCLK;
+		end if;
+   end process;
+	
+
+	process(RXCLK, RXCLK_prev)
+	begin
+		if (RXCLK = '0' and RXCLK_prev = '1') then
+			fall_RXCLK <= '1';
+		else 
+			fall_RXCLK <= '0';
+		end if;
+	end process;
+		
+	process(TXCLK, TXCLK_prev)
+	begin
+		if (TXCLK = '0' and TXCLK_prev = '1') then
+			fall_TXCLK <= '1';
+		else 
+			fall_TXCLK <= '0';
+		end if;
+	end process;
+	
+	
+	process(CLK, nRST)
+	begin
+		if (nRST = '0') then
+			TX_state <= Idle;
+			RX_state <= Idle;
 		else
-		fall_TCLK<='0';
-		end if;
-		end process;
-		
-		-- main process for transmit mode		
-		process(CLK,nRST)
-		begin
-		if nRST='0' then
-			state_current<=IDLE;
+			if (rising_edge(CLK)) then
+				case TX_state is
+					when Idle =>
+						RD <= '0';
+						if (TXDV = '1') then
+							TXD <= TX_register(7 downto 4);
+							TX_state <= First;
+							TXEN <= '1';
+						else
+							TXEN <= '0';
+						end if;
+					
+					when First =>
+						RD <= '0';
+						TXEN <= '1';
+						if (fall_TXCLK = '1') then
+							TXD <= TX_register(3 downto 0);
+							TX_state <= Second;
+						end if;
+						
+					when Second =>
+						if (fall_TXCLK = '1') then											
+							RD <= '1';
+							if (TXDV = '1') then
+								TXD <= TX_register(7 downto 4);
+								TX_state <= First;
+								TXEN <= '1';
+							else
+								TX_state <= Idle;
+								TXEN <= '0';
+							end if;
+						else
+							RD <= '0';
+						end if;
 
-		elsif (rising_edge(CLK)) then
-		
-		 case state_current is 
-			when IDLE =>
-			if TX_DV='1' then
-				state_current<=State1;
+				end case;
+							
+				case RX_state is
+					when Idle =>
+						WR <= '0';
+						if (fall_RXCLK = '1') then
+							if (RXDV = '1') then
+								RX_register(7 downto 4) <= RXD;
+								RX_state <= First;
+							end if;
+						end if;
+						
+					when First =>
+						if (fall_RXCLK = '1') then
+							if (RXDV = '1') then
+								RX_register(3 downto 0) <= RXD;
+								RX_state <= Second;
+								WR <= '1';
+							end if;
+						else
+							WR <= '0';
+						end if;
+					
+					when Second =>
+						WR <= '0';
+						if (fall_RXCLK = '1') then
+							if (RXDV = '1') then
+								RX_register(7 downto 4) <= RXD;
+								RX_state <= First;
+							else
+								RX_state <= Idle;
+							end if;
+						end if;
+						
+				end case;
 			end if;
-			
-			when State1 =>
-				
-			if fall_TCLK='1' then -- TXEN should be valid when there is falling edge of TX_CLK
-				TXEN<='1';
-				state_current<=State2;
-			end if;
-				
-			when State2 =>
-
-			if TX_DV='1' and fall_TCLK='1' then -- FIFO should be updated when there is falling edge of TX_CLK
-				RD<='1';
-				state_current<=State2;
-			elsif TX_DV='0' then --when all the data is read out
-				state_current<=IDLE;
-			end if;
-			
-			end case;
 		end if;
-		end process;
-			
+	end process;
 end Behavioral;
 
